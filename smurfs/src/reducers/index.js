59cbfd287bbe03975ef33263e1afa4e1d1f37284
Ronny Alvarado <rsalvarado777@gmail.com>
2019-04-20 11:27:52 -0500

    built out the reducer to take in the action and perform the logic dependent on action received

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,18 +1,17 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  GET_SMURF_START,
+  GET_SMURF_SUCCESS,
+  GET_SMURF_FAILURE
+} from "../actions/index";
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+};
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +20,30 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_SMURF_START: {
+      return {
+        ...state,
+        fetchingSmurfs: true
+      };
+    }
+    case GET_SMURF_SUCCESS: {
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        smurfs: action.payload
+      };
+    }
+    case GET_SMURF_FAILURE: {
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: true
+      };
+    }
+  }
+};
+
+export default rootReducer;
